3340dcf46c611c2a3f70bd2b57536cfa3e493311
Guglielmo1997 <guglielmosangalli@gmail.com>
2024-02-27 21:37:12 +0100

    added gunicorn

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 from flask_smorest import Api
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from resources.shop import blueprint as ShopBluePrint
 from resources.product import blueprint as ProductBluePrint
@@ -9,57 +10,59 @@ from db import db
 from blacklist import BLACKLIST
 import models
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    CORS(app, origin=["http://localhost:3000/"], supports_credentials=True)
 
-app.config['PROPAGATE_EXCEPTION'] = True
-app.config['API_TITLE'] = "Shops Rest Api"
-app.config['API_VERSION'] = "v1"
-app.config['OPENAPI_VERSION'] = "3.0.3"
-app.config['OPENAPI_URL_PREFIX'] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config['OPENAPI_SWAGGER_UI_URL'] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///shop.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-app.config['JWT_SECRET_KEY'] = "lear_with_Pratap"
+    app.config['PROPAGATE_EXCEPTION'] = True
+    app.config['API_TITLE'] = "Shops Rest Api"
+    app.config['API_VERSION'] = "v1"
+    app.config['OPENAPI_VERSION'] = "3.0.3"
+    app.config['OPENAPI_URL_PREFIX'] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config['OPENAPI_SWAGGER_UI_URL'] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///shop.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+    app.config['JWT_SECRET_KEY'] = "lear_with_Pratap"
 
-jwt = JWTManager(app)
+    jwt = JWTManager(app)
 
-@jwt.token_in_blocklist_loader
-def check_if_token_in_black_list(jwt_header, jwt_payload):
-    return jwt_payload['jti'] in BLACKLIST
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_black_list(jwt_header, jwt_payload):
+        return jwt_payload['jti'] in BLACKLIST
 
-@jwt.revoked_token_loader
-def revoked_token_callback(jwt_header, jwt_payload):
-    return (jsonify({"message": "The token has been revoked", "error": "token revoked"}), 401)
-
-
-@jwt.expired_token_loader
-def expired_token_callback(jwt_header, jwt_payload):
-    return (jsonify({"message":"The provided token has expired", "error":"token expired"}), 401)
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return (jsonify({"message":"Signature verification Failed", "error":"Invalid token"}), 401)
-
-@jwt.unauthorized_loader
-def missing_token_callback(error):
-    return (jsonify({"message":"Message dows not contain an access token", "error":"authoritazion required"}), 401)
-
-@jwt.needs_fresh_token_loader
-def token_not_fresh_callback(jwt_header, jwt_payload):
-    return (jsonify({"message": "The token is not fresh", "error": "fresh_token_required"}), 401)
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (jsonify({"message": "The token has been revoked", "error": "token revoked"}), 401)
 
 
-db.init_app(app)
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (jsonify({"message":"The provided token has expired", "error":"token expired"}), 401)
 
-api = Api(app)
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (jsonify({"message":"Signature verification Failed", "error":"Invalid token"}), 401)
 
-with app.app_context():
-    db.create_all()
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (jsonify({"message":"Message dows not contain an access token", "error":"authoritazion required"}), 401)
 
-api.register_blueprint(ShopBluePrint)
-api.register_blueprint(ProductBluePrint)
-api.register_blueprint(UserBluePrint)
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (jsonify({"message": "The token is not fresh", "error": "fresh_token_required"}), 401)
+
+
+    db.init_app(app)
+
+    api = Api(app)
+
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(ShopBluePrint)
+    api.register_blueprint(ProductBluePrint)
+    api.register_blueprint(UserBluePrint)
 
 #if __name__ == '__main__':
     #app.run()
